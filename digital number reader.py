d41fabac4258b974a9d907a408e7c18dbeb68a4a
SkyLime <Skyime@users.noreply.github.com>
2018-08-21 16:59:36 +0100

    Update digital number reader.py
    #  added comments

--- digital number reader.py
+++ digital number reader.py
@@ -1,3 +1,4 @@
+#  conversion list
 numbers = [
     ' _ | ||_|',
     '     |  |',
@@ -10,13 +11,19 @@ numbers = [
     ' _ |_||_|',
     ' _ |_| _|'
     ]
+
+#  read the challenge text file, adding each line into a list with no \n where new lines in the file would be, then close the file
 chall_text = [w.replace('\n', '') for w in open('digital number challenges.txt', 'r').readlines()]
 open('digital number challenges.txt', 'r').close()
+
+#  take the characters for a single digit over its 3 lines, and add them in their own order as a string, similar to the conversion list
 lines_ordered = ''
 for i_0 in range(0, 10, 3):
     for i_1 in range(0, 25, 3):
         for i_2 in range(0, 3):
             lines_ordered += chall_text[i_0:(i_0 + 3)][i_2][i_1:(i_1 + 3)]
+
+#  check each digit's characters against the conversion list and output as an integer number
 results = []
 for i_3 in range(0, 316, 9):
     [results.append(numbers.index(each)) for each in numbers if lines_ordered[i_3:(i_3 + 9)] == each]
